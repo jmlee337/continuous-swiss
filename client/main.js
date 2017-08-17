744a202487004d3acd04a3b096d237c61343dc9c
Jeremy Lee <jmlee337@gmail.com>
2017-08-17 01:34:46 -0700

    Allow adding players and clearing the whole thing

--- client/main.js
+++ client/main.js
@@ -5,7 +5,18 @@ import { Players } from '../lib/collections.js';
 import './main.html';
 
 Template.body.helpers({
-	players() {
+	'players': function() {
     return Players.find({});
+  },
+});
+
+Template.body.events({
+  'submit .addPlayer': function(event) {
+    Players.insert({name: event.target.playerName.value});
+    return false;
+  },
+
+  'click .clear': function(event) {
+    Meteor.call('clearDb');
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,1 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+
 import { Players } from '../lib/collections.js';
+
+Meteor.methods({
+	clearDb: function() {
+		Players.remove({});
+	},
+});
