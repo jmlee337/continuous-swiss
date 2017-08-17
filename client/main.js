24cc3e80082e48dd9ec992239651906c8bbd6595
Jeremy Lee <jmlee337@gmail.com>
2017-08-17 02:20:21 -0700

    Add the four queues

--- client/main.js
+++ client/main.js
@@ -1,18 +1,33 @@
 import { Template } from 'meteor/templating';
 
 import { Players } from '../lib/collections.js';
+import { Queue } from '../lib/queue.js';
 
 import './main.html';
 
 Template.body.helpers({
-	'players': function() {
-    return Players.find({});
+	'unqueued': function() {
+    return Players.find({queue: Queue.NONE});
+  },
+
+  'matchmaking': function() {
+    return Players.find({queue: Queue.MATCHMAKING});
+  },
+
+  'waiting': function() {
+    return Players.find({queue: Queue.WAITING});
+  },
+
+  'playing': function() {
+    return Players.find({queue: Queue.PLAYING});
   },
 });
 
 Template.body.events({
   'submit .addPlayer': function(event) {
-    Players.insert({name: event.target.playerName.value});
+    let playerName = event.target.playerName;
+    Meteor.call('addPlayer', playerName.value);
+    playerName.value = "";
     return false;
   },
 
--- server/main.js
+++ server/main.js
@@ -1,8 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Players } from '../lib/collections.js';
+import { Queue } from '../lib/queue.js';
 
 Meteor.methods({
+  addPlayer: function(playerName) {
+    Players.insert({
+      name: playerName,
+      queue: Queue.NONE,
+    });
+  },
+
 	clearDb: function() {
 		Players.remove({});
 	},
