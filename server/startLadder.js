b7c14864ed3f875f6173b477f8e661e940f7d823
Jeremy Lee <jmlee337@gmail.com>
2017-09-15 21:59:27 -0700

    do better initial line up
    fix #47

--- server/startLadder.js
+++ server/startLadder.js
@@ -9,18 +9,27 @@ Meteor.methods({
   startLadder: function(ladderId) {
     check(ladderId, String);
 
-    const playersCursor = Players.find({ladderId: ladderId}, SeedsOptions);
-    playersCursor.forEach((player, i) => {
-      if (player.seed === Number.MAX_SAFE_INTEGER) {
-        Players.update(player._id, {$set: {seed: i, queueTime: Date.now()}});
-      } else {
-        if (player.seed !== i) {
-          throw new Meteor.Error('INTERNAL', 'gap in seeds somehow');
-        }
-        Players.update(player._id, {$set: {queueTime: Date.now()}});
-      }
-    });
+    const players = Players.find({ladderId: ladderId}, SeedsOptions).fetch();
+    const middle = Math.floor(players.length / 2);
+    for (let i = 0; i < middle; i++) {
+      updatePlayer(players[i], i);
+    }
+    for (let i = players.length - 1; i >= middle; i--) {
+      updatePlayer(players[i], i);
+    }
     Ladders.update(
-        ladderId, {$set: {started: true, numSeeds: playersCursor.count()}});
+        ladderId, {$set: {started: true, numSeeds: players.length}});
   },
 });
+
+function updatePlayer(player, i) {
+  console.log(player.name);
+  if (player.seed === Number.MAX_SAFE_INTEGER) {
+    Players.update(player._id, {$set: {seed: i, queueTime: Date.now()}});
+  } else {
+    if (player.seed !== i) {
+      throw new Meteor.Error('INTERNAL', 'gap in seeds somehow');
+    }
+    Players.update(player._id, {$set: {queueTime: Date.now()}});
+  }
+}
