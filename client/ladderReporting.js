3268c3815225230ca2400f41eb4a5003b9cf5218
Jeremy Lee <jmlee337@gmail.com>
2017-09-20 22:21:51 -0700

    finish reporting interface
    close #49

--- client/ladderReporting.js
+++ client/ladderReporting.js
@@ -37,6 +37,10 @@ Template.ladderReporting.helpers({
   'pairing': function() {
     return Pairings.findOne(Template.instance().dict.get('pairingId'));
   },
+
+  'submitError': function() {
+    return Template.instance().dict.get('submitError');
+  }
 });
 
 Template.ladderReporting.events({
@@ -44,10 +48,53 @@ Template.ladderReporting.events({
     templateInstance.dict.set('pairingId', event.currentTarget.id);
   },
 
+  'click .cancel': function(event, templateInstance) {
+    templateInstance.dict.set('pairingId', undefined);
+  },
+
   'submit #form': function(event, templateInstance) {
-    console.log(event.target.winner.value);
-    console.log(event.target.player1.value);
-    console.log(event.target.player2.value);
+    const winner = event.target.winner.value;
+    if (!winner) {
+      templateInstance.dict.set('submitError', 'select a winner!');
+      return false;
+    }
+
+    const ladderId = Ladders.findOne()._id;
+    const pairingId = event.target.pairingId.value;
+    const winnerInt = winner === '2' ? 2 : 1;
+
+    const pairing = Pairings.findOne(pairingId);
+    const player1Method =
+        event.target.player1.value === 'yes' ? 'queuePlayer' : 'unqueue';
+    const player2Method =
+        event.target.player2.value === 'yes' ? 'queuePlayer' : 'unqueue';
+
+    Meteor.call('submitWinner', ladderId, pairingId, winnerInt, (err) => {
+      if (err && err.error !== 'PRECONDITION_FAILED') {
+        const errMsg = 'Ask for help: ' + err.error + ', ' + err.reason;
+        templateInstance.dict.set('submitError', errMsg);
+      } else {
+        templateInstance.dict.set('submitError', undefined);
+        Meteor.call(player1Method, ladderId, pairing.player1Id, (err) => {
+          if (err) {
+            const originalErr = templateInstance.dict.get('submitError');
+            templateInstance.dict.set(
+                'submitError',
+                originalErr ? originalErr + '\n' : '' + 'Ask for help: ' +
+                    pairing.player1Name + ', ' + err.error + ', ' + err.reason);
+          }
+        });
+        Meteor.call(player2Method, ladderId, pairing.player2Id, (err) => {
+          if (err && err.error !== 'PRECONDITION_FAILED') {
+            const originalErr = templateInstance.dict.get('submitError');
+            templateInstance.dict.set(
+                'submitError',
+                originalErr ? originalErr + '\n' : '' + 'Ask for help: ' +
+                    pairing.player2Name + ', ' + err.error + ', ' + err.reason);
+          }
+        });
+      }
+    });
     return false;
   },
 });
